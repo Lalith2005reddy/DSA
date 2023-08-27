c358a9f074c1903c5b9c0b1fb64407c7f4e4ed96
Lalith2005reddy <125805779+Lalith2005reddy@users.noreply.github.com>
2023-08-27 23:12:34 +0530

    File Updation in arrays

--- Arrays/Sorting_array.cpp
+++ Arrays/Sorting_array.cpp
@@ -22,7 +22,8 @@ int main()
       
     for(int i=0; i<n; i++)
     {
-        for(int j=i+1; j<n; j++) { if(a[i]>a[j])
+        for(int j=i+1; j<n; j++) { 
+            if(a[i]>a[j])
             {
                 int temp = a[i];
                 a[i] = a[j];
