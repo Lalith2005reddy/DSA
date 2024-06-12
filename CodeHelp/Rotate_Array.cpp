bcc126fbb62719df4dd9057f7303d8c8fef3b325
Lalith2005reddy <125805779+Lalith2005reddy@users.noreply.github.com>
2024-06-12 18:29:03 +0530

    Rotate_Array

---
+++ CodeHelp/Rotate_Array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> temp(nums.size());
+        int i=0;
+        while(i != nums.size()){
+            temp[(i+k)%nums.size()]= nums[i];
+            i++;
+        }
+        nums = temp;
+
+    }
+};
